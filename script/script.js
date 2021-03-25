9f5d656c4795645d42d5a5c72518ba0eb337e397
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-03-24 23:36:02 -0300

    Aplicando a responsividade

--- script/script.js
+++ script/script.js
@@ -4,18 +4,32 @@ const buttonShare = window.document.getElementById('share');
 
 panelShare.style.display = 'none';
 
-const aplicarFuncao = function (button, callback) {
+const aplicarClick = function (button, callback) {
     button.onclick = callback;
 }
 
-aplicarFuncao(buttonShare, () => {
-    if(panelShare.style.display == 'none') {
-        panelShare.style.display = 'block';
-        buttonShare.style.color = 'white';
-        buttonShare.style.backgroundColor = '#48556a';
+const mediaQuerieList = matchMedia("(max-width: 758px)");
+
+const aplicarInteracaoResponsiva = function (mediaQuerieList) {
+    if(mediaQuerieList.matches) {
+        // aplicarClick(buttonShare, () => {
+
+        // })
     } else {
-        panelShare.style.display = 'none';
-        buttonShare.style.color = '';
-        buttonShare.style.backgroundColor = '';
     }
-})
+    aplicarClick(buttonShare, () => {
+        if(panelShare.style.display == 'none') {
+            panelShare.style.display = 'block';
+            buttonShare.style.color = 'white';
+            buttonShare.style.backgroundColor = '#48556a';
+        } else {
+            panelShare.style.display = 'none';
+            buttonShare.style.color = '';
+            buttonShare.style.backgroundColor = '';
+        }
+    });
+}
+
+aplicarInteracaoResponsiva(mediaQuerieList);
+
+mediaQuerieList.addListener(aplicarInteracaoResponsiva);
